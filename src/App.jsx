0bd5bb778f6860dd6a3245d837d4678622cf84a7
vivekop0 <sarkarjii534@gamil.com>
2024-02-08 16:43:00 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import { RevenueCard } from './components/RevenueCard'
+
+function App() {
+return <>
+<RevenueCard title={"Amount pending"} amount={"92,312.20"} orderCount={13}></RevenueCard>
+
+
+</>
+}
+
+export default App
